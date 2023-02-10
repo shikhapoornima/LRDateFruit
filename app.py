4dc7a579d4b0fa32ecfe2a4309817469d3d03a38
shikhapoornima <91452606+shikhapoornima@users.noreply.github.com>
2023-02-10 15:56:49 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 10 15:23:24 2023
+
+@author: admin
+"""
+
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+
+import pickle
+
+
+app = Flask(__name__)
+model = pickle.load(open('Assignmentlinear.pkl','rb')) 
+
+
+@app.route('/')
+def home():
+  
+    return render_template("index.html")
+  
+@app.route('/predict',methods=['GET'])
+def predict():
+    
+    
+    '''
+    For rendering results on HTML GUI
+    '''
+
+    
+    area = float(request.args.get('area'))
+    perimeter = float(request.args.get('perimeter'))
+    prediction =int(model.predict([[area,perimeter]]))
+    return render_template('index.html', prediction_text=prediction)
+
+
+app.run()
